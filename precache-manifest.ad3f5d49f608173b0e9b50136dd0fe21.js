a984622be7a16d44f150bda7c2cf66d5b0b8dc81
Álvaro Zájara <alzca1@gmail.com>
2020-08-23 12:50:18 +0200

    Updates

---
+++ precache-manifest.ad3f5d49f608173b0e9b50136dd0fe21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd43a64dad920861d9a4b0f88231de30",
+    "url": "/RetroTodoApp/index.html"
+  },
+  {
+    "revision": "b0144ca0d4d1ab546df9",
+    "url": "/RetroTodoApp/static/css/main.911795a7.chunk.css"
+  },
+  {
+    "revision": "ac4401f07f9d283dbf4c",
+    "url": "/RetroTodoApp/static/js/2.e68c549c.chunk.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/RetroTodoApp/static/js/2.e68c549c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0144ca0d4d1ab546df9",
+    "url": "/RetroTodoApp/static/js/main.9bc328c9.chunk.js"
+  },
+  {
+    "revision": "84ddc3ea589d836ece0d",
+    "url": "/RetroTodoApp/static/js/runtime-main.031e0c7b.js"
+  }
+]);
